285aae012deec7fd70dee41e71b64f6839a3a771
doquanghuy2778 <dohuy0079@gmail.com>
2023-08-17 19:28:41 +0700

    first commit

---
+++ HackerRank_DS1/HackerRank_DS1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HackerRank_DS1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] b = new int[a];
+            string input = Console.ReadLine();
+            string[] inputValue = input.Split(' ');
+
+            for (int i = 0; i < a; i++)
+            {
+                if (int.TryParse(inputValue[i], out int value))
+                {
+                    b[i] = value;
+                }
+            }
+
+            for (int i = b.Length - 1; i >= 0; i--)
+            {
+                Console.Write(b[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
